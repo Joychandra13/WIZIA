2af785dade2c4c88a7a315912cdad6b8e2a57c7d
Joychandra13 <joychandra1313@gmail.com>
2024-11-27 17:26:09 +0600

    12

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 function Footer() {
   return (
     <>
-       <footer className='flex items-center justify-between px-[120px] py-[40px]'>
+       <footer className='flex flex-col md:flex-row items-center md:justify-between md:px-[120px] px-6 md:py-[40px] py-10'>
             <img src="/Logo.svg" alt="" />
-            <h1 className='font-light text-secondary text-base'>Copyright © 2023 Wizia. All rights reserved.</h1>
+            <h1 className='font-light text-secondary text-base text-center md:text-start pt-[24px] md:pt-0'>Copyright © 2023 Wizia. All rights reserved.</h1>
        </footer>
     </>
   )
