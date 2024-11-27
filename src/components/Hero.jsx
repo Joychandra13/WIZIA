ad34d4f8df1c427ff117f93374cd323368234ef3
Joychandra13 <joychandra1313@gmail.com>
2024-11-27 17:33:24 +0600

    13

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,14 +7,14 @@ function Hero() {
     <>
         <section className=" flex items-center md:bg-[url('/Hero.svg')] bg-[url('/Hero2.svg')] bg-cover bg-center bg-no-repeat pt-[44px] pb-[72px] px-4 md:py-28 md:px-32 ">
         
-            <div className="space-y-[8px] w-[80%] text-center md:text-left mx-auto">
+            <div className="space-y-[8px] w-full text-center md:text-left mx-auto">
                 <h1 className='text-primary italic font-Mont md:text-2xl text-base'>AI SDRs (aiDRs)</h1>
 
                 <h1 className="font-bold md:text-5xl text-[40px] font-Mont text-white "> 
                     More <span className='font-normal italic'>sleads</span>, <br /> less <span className='font-normal italic' style={{ letterSpacing: '0.1em',  }}>people</span>.
                 </h1>
 
-                <p className="text-secondary md:w-[450px] md:text-xl text-lg font-WorkSans"> 
+                <p className="text-secondary md:w-[450px] md:text-xl text-lg font-light"> 
                     Train an aiDR on your ICP and messaging matrix. Activate it on a patch. It will send personalized sequences to every target contact.
                 </p>
                 
